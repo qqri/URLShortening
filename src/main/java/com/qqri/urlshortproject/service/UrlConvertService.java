e0491475254b63058407325e6f4af0f1570ebb4a
qqri <50269335+qqri@users.noreply.github.com>
2020-12-17 16:40:23 +0900

    delete unused files

--- src/main/java/com/qqri/urlshortproject/service/UrlConvertService.java
+++
@@ -1,9 +0,0 @@
-package com.qqri.urlshortproject.service;
-
-import com.qqri.urlshortproject.service.dto.ShortUrlResult;
-
-
-public interface UrlConvertService {
-
-    ShortUrlResult getShortenUrl(String url);
-}
--- src/main/java/com/qqri/urlshortproject/service/dto/ShortUrlType.java
+++
@@ -1,5 +0,0 @@
-package com.qqri.urlshortproject.service.dto;
-
-public enum ShortUrlType{
-    ORIGIN,SHORT;
-}
